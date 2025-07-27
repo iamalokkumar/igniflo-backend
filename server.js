55b6e36be7cfef3dae75d3fe10ffb312f47cd257
Alok Kumar <alokkumardasd2016@gmail.com>
2025-07-27 18:03:24 +0530

    changes

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ const server = http.createServer(app);
 // Configure Socket.io
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT'],
+    credentials: true,
   },
 });
 
